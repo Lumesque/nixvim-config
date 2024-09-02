fcb95fff41dc7e6ce3105c86b4a7d423b487676d
Joshua Luckie <luckie.joshua.c@gmail.com>
2024-09-01 21:31:24 -0400

    Updating so that it will fail if anything actually occurs

--- _checks/plugins_used.sh
+++ _checks/plugins_used.sh
@@ -7,5 +7,5 @@ for i in $(/bin/ls $FILEDIR/ | grep -v "default.nix"); do
     ! grep -q "$i" "$DEFAULT_FILE" && unfound_strings+="$i,"
 done
 
-[ -n "$unfound_strings" ] && echo "Could not find ${unfound_strings:0:-1} in $DEFAULT_FILE" >&2 && exit 1
-exit 0
+[ -n "$unfound_strings" ] && echo "Could not find ${unfound_strings:0:-1} in $DEFAULT_FILE" >&2 && echo "Failures Found" && exit 1
+echo "All tests passed!" && exit 0
